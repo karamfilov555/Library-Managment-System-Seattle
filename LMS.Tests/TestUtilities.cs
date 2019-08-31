cc6920a154516affd6f834f39c2f4a851ecd5c81
cool_tool@yahoo.com <cool_tool@yahoo.com>
2019-08-31 19:04:03 +0300

    InMemory Test assembly added firsttest impld

---
+++ LMS.Tests/TestUtilities.cs
@@ -0,0 +1,15 @@
+﻿using LMS.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Tests
+{
+    public static class TestUtilities
+    {
+        public static DbContextOptions<LMSContext> GetOptions(string databaseName)
+        {
+            return new DbContextOptionsBuilder<LMSContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+        }
+    }
+}
