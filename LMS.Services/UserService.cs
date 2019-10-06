482389ef4498f75c06b642cefe3e81841f666b63
nikola.rangelov@abv.bg <nikola.rangelov@abv.bg>
2019-10-06 03:03:06 +0300

    UserService FxCopFixes

--- LMS.Services/UserService.cs
+++ LMS.Services/UserService.cs
@@ -33,36 +33,36 @@ namespace LMS.Services
         //    //return user;
         //}
         public async Task<ICollection<User>> GetUsersAsync()
-            => await _context.Users.ToListAsync();
+            => await _context.Users.ToListAsync().ConfigureAwait(false);
 
         public async Task<Role> GetUserRoleAsync(string userId)
         {
-            var roleId = await _context.UserRoles.FindAsync(userId);
-            var role = await _context.Roles.FindAsync(roleId);
+            var roleId = await _context.UserRoles.FindAsync(userId).ConfigureAwait(false);
+            var role = await _context.Roles.FindAsync(roleId).ConfigureAwait(false);
             return role;
         }
 
         public async Task<User> FindUserByIdAsync(string userId)
-            => await _context.Users.FindAsync(userId);
+            => await _context.Users.FindAsync(userId).ConfigureAwait(false);
 
         public async Task<User> FindUserForBanAsync(string userId)
         {
             var user = await _context.Users
                 .Include(u => u.BanRecord)
-                .FirstOrDefaultAsync(m => m.Id == userId);
+                .FirstOrDefaultAsync(m => m.Id == userId).ConfigureAwait(false);
             return user;
         }
         public async Task<string> FindUsernameByIdAsync(string userId)
         {
             var user = await _context.Users
                 .Include(u => u.BanRecord)
-                .FirstOrDefaultAsync(m => m.Id == userId);
+                .FirstOrDefaultAsync(m => m.Id == userId).ConfigureAwait(false);
             return user.UserName;
         }
         public async Task<User> BanUserAsync(BanDto banDto)
         {
-            var user = await FindUserForBanAsync(banDto.UserId);
-            var ban = await _banService.AddBan(banDto);
+            var user = await FindUserForBanAsync(banDto?.UserId).ConfigureAwait(false);
+            var ban = await _banService.AddBan(banDto).ConfigureAwait(false);
 
             return user;
         }
@@ -70,22 +70,22 @@ namespace LMS.Services
         {
             var user = await _context.Users
                 .Include(u => u.BanRecord)
-                .FirstOrDefaultAsync(m => m.UserName == username);
+                .FirstOrDefaultAsync(m => m.UserName == username).ConfigureAwait(false);
             return user;
         }
         public async Task<User> GetAdmin()
         {
-            var adminRole = await _context.Roles.FirstAsync(role => role.Name.ToLower() == "admin");
-            var adminId = await _context.UserRoles.FirstAsync(roleId => roleId.RoleId == adminRole.Id);
-            var admin = await FindUserByIdAsync(adminId.UserId);
+            var adminRole = await _context.Roles.FirstAsync(role => role.Name.ToLower() == "admin").ConfigureAwait(false);
+            var adminId = await _context.UserRoles.FirstAsync(roleId => roleId.RoleId == adminRole.Id).ConfigureAwait(false);
+            var admin = await FindUserByIdAsync(adminId.UserId).ConfigureAwait(false);
             return admin;
         }
         public async Task DeleteUserAsync(string id)
         {
-            var user = await FindUserByIdAsync(id);
-            await DeleteAllNotificationsOfUser(id);
+            var user = await FindUserByIdAsync(id).ConfigureAwait(false);
+            await DeleteAllNotificationsOfUser(id).ConfigureAwait(false);
             _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync().ConfigureAwait(false);
         }
         private async Task DeleteAllNotificationsOfUser(string userId)
         {
